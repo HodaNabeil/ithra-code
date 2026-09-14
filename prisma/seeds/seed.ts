be771225e066905b12a8c337af1ea432f8d9d019
Hoda Nabeil <hodanabeil67@gmail.com>
2026-09-14 07:35:54 +0300

    feat(seed): set engineering course price to 20

--- prisma/seeds/seed.ts
+++ prisma/seeds/seed.ts
@@ -164,7 +164,7 @@ async function main() {
       thumbnailUrl:
         'https://images.unsplash.com/photo-1517048676732-d65bc937f952?w=800',
       previewVideo: null,
-      price: 1,
+      price: 20,
       compareAtPrice: 499.0,
       currency: 'USD',
       level: CourseLevel.INTERMEDIATE,
@@ -232,7 +232,7 @@ async function main() {
       thumbnailUrl:
         'https://images.unsplash.com/photo-1517048676732-d65bc937f952?w=800',
       previewVideo: null,
-      price: 1,
+      price: 20,
       compareAtPrice: 499.0,
       currency: 'USD',
       level: CourseLevel.INTERMEDIATE,
